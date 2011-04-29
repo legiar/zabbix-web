1b84e9a8d7618d567aa7782b58d616b86a604108
Mikhail Mikhaliov <legiar@gmail.com>
2011-04-29 14:12:31 +0300

    Empty application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zabbix::Application.config.secret_token = '7764ad1257a5822373c602d020e4b49aa4747406ba31f1f2917f69dbb64396b025882904437de84ae0fc59eca78b913f6c73238012ac71a7701ef1cf064a69a2'
